0283b4112ddd01cd4a418047deade4a42f0e425e
bettybop29 <babyimoneter@gmail.com>
2022-02-26 18:12:00 +0700

    done landing page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,38 +1,42 @@
 <template>
-<div class="bg-blue-white">
+<div class="bgwhite">
   <div class="home">
     <h1>Find The Best Fit <br> For Your Team</h1>
     <img alt="Vue logo" src="../assets/banner.png">      
   </div>
-  
-  <h2>How can we help your business</h2>
   </div>
-  <div class="bg-black-blue">
+  <div class="bg-gradient">
+  <h2>How can we help your business</h2>
+ 
+  
   <div class="main-card">
     <div class="card">
       <img src="../assets/icon/CREDIT.svg" alt="">
       <div class="card-title">Credits</div>
       <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
         Aliquam tenetur accusantium earum ab nisi aut,
-        Id, veniam provident autem est quam enim.</P>
+        Id, veniam provident autem est quam enim. veniam provident autem est quam enim.
+        veniam provident autem est quam enim.veniam provident autem est quam enim.</P>
     </div>
     <div class="card">
       <img src="../assets/icon/POSTJOB.svg" alt="">
       <div class="card-title">Post Jobs</div>
       <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
         Aliquam tenetur accusantium earum ab nisi aut,
-        Id, veniam provident autem est quam enim.</P>
+        Id, veniam provident autem est quam enim.autem est quam enim. veniam provident autem est quam enim.
+        veniam provident autem est quam enim.veniam provident autem est quam enim.</P>
     </div>
     <div class="card">
       <img src="../assets/icon/INTERVIEW.svg" alt="">
       <div class="card-title">interview Schedule</div>
       <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
         Aliquam tenetur accusantium earum ab nisi aut,
-        Id, veniam provident autem est quam enim.</P>
+        Id, veniam provident autem est quam enim.autem est quam enim. veniam provident autem est quam enim.
+        veniam provident autem est quam enim.veniam provident autem est quam enim.</P>
     </div>
   </div>
-  </div>
-  <div class="bg-black">
+  
+  
   <h2>Our Satisfied Partners</h2>
   <div class="sponsor">
     <img src="../assets/sponsor/BNI.png" alt="">
@@ -42,7 +46,38 @@
     <img src="../assets/sponsor/SHARP.png" alt="">
     <img src="../assets/sponsor/ANA.png" alt="">
   </div>
+
+<!-- footer -->
+<footer>
+  <div class="container">
+  <p class="logo-footer">Cariin kerja</p>
+    <h4>Jl. Sudirman Jakarta Pusat</h4>
   </div>
+      <div class="content">
+        <ul>Company</ul>
+        <li>About US</li>
+        <li>Blog</li>
+        <li>Carrer</li>
+      </div>
+      <div class="content">
+        <ul>Features</ul>
+        <li>Review</li>
+        <li>Community</li>
+        <li>Social Media</li>
+      </div>
+      <div class="content">
+        <ul>Help</ul>
+        <br>
+        <li>Support@gmail.com</li>
+        <li>+628653240093</li>
+      </div>
+ 
+ 
+ 
+</footer>
+<hr>
+<h5>2022 Copyright Connect. All Rights reserved</h5>
+</div>
 </template>
 
 <script>
@@ -61,17 +96,19 @@ export default {
   align-items: center;
   justify-content: space-between;
   margin: 0;
-  
+}
+.home img {
+  margin-right: 30px;
 }
 h1 {
   text-align: left;
-  margin-left: 40px;
+  margin-left: 60px;
   font-size: 50px;
 }
 h2 {
   padding-top: 90px;
   font-size: 35px;
-  padding-bottom: 40px;
+  padding-bottom: 80px;
   color: white;
   
 }
@@ -108,6 +145,45 @@ h2 {
   margin: 20px;
 }
 
+footer {
+  text-align: left;
+  text-decoration: none;
+  margin: 0;
+  padding: 20px;
+  color: white;
+  background-color: #101419; 
+  padding-top: 90px; 
+}
+footer p {
+  font-size: 30px;
+}
+footer ul {
+  font-size: 20px;
+  font-weight: 700;
+  padding-bottom: 10px;
+}
+footer li {
+  padding: 4px;
+}
+.content {
+  display: inline-block;
+  list-style-type: none;
+  margin-left: 350px;
+}
+
+h5 {
+  background-color: #101419;
+  padding: 30px;
+  color: white;
+}
+
+
+
+.logo-footer {
+  padding-bottom: 20px;
+}
+
+
 /* colorbg-gradient */
 
 .bg-black {
@@ -118,6 +194,12 @@ h2 {
 }
 .bg-blue-white {
   background-image: linear-gradient(white,#2e2e78 );
+}
+.bg-gradient {
+  background-image: linear-gradient(#2E2E78, #101419 50%);
+}
+.bgwhite {
+  background-image: linear-gradient(#ffffff, #2E2E78);
 }
 
 </style>
